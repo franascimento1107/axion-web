dffe4e96c0bae3742534e8e2dde1dca0ba165eac
Francisco Augusto Franco do Nascimento <hm4q@petrobras.com.br>
2021-09-24 11:20:16 -0300

    Fontes iniciais

---
+++ axionweb/src/main/java/com/unitech/axionweb/AxionwebApplication.java
@@ -0,0 +1,13 @@
+package com.unitech.axionweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxionwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxionwebApplication.class, args);
+	}
+
+}
